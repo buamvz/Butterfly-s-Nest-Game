e005d9635ae888c6c4cc0c047dcfdd2001f41125
SiennaHa <sienna.hargreaves31@gmail.com>
2025-10-02 17:00:27 +1300

    dialogue scene 1 and light

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -35,7 +35,6 @@ public class Dialogue : MonoBehaviour
     private void ToggleWindow(bool show)
     {
         Debug.Log("show/hide");
-        window.SetActive(show);
         text.SetActive(show);
 
     }
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogueScript;
+
+    [SerializeField] BoxCollider2D colliderInteraction;
+    [SerializeField] int dialogueStartLine;
+    
+   
+    void Update()
+    {
+        if (dialogueScript.waiting == true)
+            return;
+
+        if (Input.GetMouseButtonDown(0) && dialogueScript.waiting == false)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null && hit.collider == colliderInteraction)
+            {
+                dialogueScript.indexStart = dialogueStartLine;
+                //dialogueScript.indexEnd = dialogueEndLine;
+                dialogueScript.StartDialogue();
+            }
+            else
+                dialogueScript.EndDialogue(); 
+        }
+
+
+    }
+
+}
