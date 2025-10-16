a1e96d869779f012e45665e543cb3db3a01b5a8c
buamvz <lovebrooke321@gmail.com>
2025-10-16 13:45:21 +1300

    Brooke Push 3 - 16/10/25

--- Assets/Scripts/InteractionBlocker.cs
+++ Assets/Scripts/InteractionBlocker.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class InteractionBlocker : MonoBehaviour
 {
+    [SerializeField] BoxCollider2D blocker;
+
     void OnMouseDown()
     {
-        //absorbs clicks
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+
+        if (hit.collider == blocker)
+        {
+            return;
+            Debug.Log("Hit blocker");
+
+        }
     }
 }
