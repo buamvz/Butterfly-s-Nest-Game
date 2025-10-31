2044cf3145a6ff9ee6c26652d11a330ff9d58343
buamvz <lovebrooke321@gmail.com>
2025-11-01 00:06:32 +1300

    Brooke Push 6 - 31/10/25

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public static AudioScript instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
